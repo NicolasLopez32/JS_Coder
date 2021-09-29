84a6da61ee3ec728eeaedbc9dd1fcc0688005688
Nicolas Lopez <nicolaslopez3260@gmail.com>
2021-09-29 20:54:47 -0300

    desafio2

---
+++ Clase-1/main.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Ingresá tu nombre:");
+let apellido = prompt("Ingresá tu apellido:");
+
+alert("Tu nombre es: "+ nombre + " " + apellido); 
---
+++ Clase-2/main.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad");
+
+
+// Si la variable nombre y edad estan vacios, no muestro ningun mensaje
+// Luego, verifico si la edad es mayor o menor a 18, y muestro un mensaje de acuerdo a eso.
+if((nombre !="") && (edad !="")) {
+    if (edad > 18) {
+        alert("Hola " +nombre + " \nBienvenido!")
+    } else {
+        alert("Lo lamentamos, " + nombre + "\nDebes ser mayor de edad para acceder a éste sitio")
+    }
+} else {
+    alert(" El nombre y edad son obligatorios.")
+}
+
